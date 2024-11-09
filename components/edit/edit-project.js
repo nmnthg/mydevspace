94be17a6ad58dfb62bb54970ea96687ac6a96b65
nmnthg <77082885+nmnthg@users.noreply.github.com>
2024-11-08 21:58:57 -0500

    Revert "pdf-view component"
    This reverts commit 9629036f2ed5d7e4b13328733557315a1e405e7c.

--- components/edit/edit-project.js
+++ components/edit/edit-project.js
@@ -130,7 +130,7 @@ function EditProject({ projectId }) {
                     alt={project.name}
                     width={400}
                     height={250}
-                    className="object-cover rounded-sm"
+                    className="object-cover rounded-t-lg"
                   />
                 </div>
               </div>
--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "lvhyctrolcfedujsavhj.supabase.co"
-      }
-    ],
-  },
+    images: {
+        domains: ['lvhyctrolcfedujsavhj.supabase.co']
+    }
 };
 
 export default nextConfig;
