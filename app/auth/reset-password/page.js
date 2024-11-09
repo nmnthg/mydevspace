17703ead639dfc574876bfd1b0749cf8b3db9582
nmnthg <77082885+nmnthg@users.noreply.github.com>
2024-11-09 01:01:54 -0500

    route protection for reset-password

--- app/auth/reset-password/page.js
+++ app/auth/reset-password/page.js
@@ -1,8 +1,24 @@
+'use client'
 import ResetPasswordForm from "@/components/auth/reset-password-form";
 import React from "react";
-
+import { useEffect } from "react";
+import { useUser } from "@/context/user.context";
+import { useRouter } from 'next/navigation';
 
 const ResetPasswordPage = () => {
+  const { user } = useUser()
+  const router = useRouter(); 
+
+  useEffect(() => {
+    if (!user) {
+      router.push('/auth');
+    }
+  }, [user, router]);
+
+  if (!user) {
+    return null; 
+  }
+  
   return (
        <ResetPasswordForm/> 
   );
